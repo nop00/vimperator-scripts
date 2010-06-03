c2d4561d2696ab65d70d6a56f68b55d09c9432a7
nop <nop@peuw.net>
2010-06-04 00:09:13 +0200

    Added completion support for tags

--- plugins/delicious.js
+++ plugins/delicious.js
@@ -1,12 +1,12 @@
 // Vimperator Plugin: 'Delicious'
-// Last Change: 25-Aug-2009
+// Last Change: 02-Jun-2010
 // License: MIT
 // Maintainer: Travis Jeffery <travisjeffery@gmail.com>
 // Usage: Use :delicious "description in quotes (optional)" <tags delimited by spaces> command
 // Usage: if successfully posted you will see "done" echoed
 
 commands.addUserCommand(['delicious'], "Save page as a bookmark on Delicious",
-    function(args) {
+   function(args) {
         var url = "https://api.del.icio.us/v1/posts/add?";
         url += "&url=" + encodeURIComponent(buffer.URL);
         url += "&description=" + encodeURIComponent(buffer.title);
@@ -26,5 +26,25 @@ commands.addUserCommand(['delicious'], "Save page as a bookmark on Delicious",
         var status = xml.getElementsByTagName('result')[0].getAttribute('code');
 
         liberator.echo(status);
+    },
+    {
+        argCount: '*',
+        completer: function (context, args) {
+            var url = "https://api.del.icio.us/v1/tags/get";
+            var xhr = new XMLHttpRequest();
+            xhr.open("POST", url, false);
+            xhr.send(null);
+            var xml = (new DOMParser()).parseFromString(xhr.responseText, "text/xml");
+            var tags = xml.getElementsByTagName('tag');
+            var values = Array();
+
+            for(var i = 0; i < tags.length; ++i)
+            {
+                values.push([tags[i].getAttribute('tag'), tags[i].getAttribute('count')]);
+            }
+
+            context.title = ['Tags', 'Bookmark count'];
+            context.completions = values;
+        }
     }
 );
